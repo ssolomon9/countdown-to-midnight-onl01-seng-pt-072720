4a628c790ef9e690e10e6b96128ac77e406a0384
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-10 03:40:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  def countdown_with_sleep(sleep)
+      sleep 5
+  end
+    while number > 0 
+    countdown_with_sleep(5)
+      puts "#{number} SECOND(S)!"
+      number -= 1 
+    end  
+    return "HAPPY NEW YEAR!"
 end
