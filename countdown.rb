3412a651bc09aec32c31c809e1e16dfe83bcb916
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-10 03:36:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,9 @@
 #write your code here
 
 def countdown(number)
-  
+  def countdown_with_sleep(sleep)
+      sleep 5
+    end
     while number > 0 
     countdown_with_sleep(5)
       puts "#{number} SECOND(S)!"
