4352701d00d5bcee119f0e1b937a8214059c70b7
Sekarri Solomon <ssolomonewsis@gmail.com>
2020-08-10 03:33:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,14 @@
 #write your code here
-require 'pry'
+
 def countdown(number)
     def countdown_with_sleep
       sleep 1
     end
+    
     while number > 0 
     
       puts "#{number} SECOND(S)!"
       number -= 1 
     end  
     return "HAPPY NEW YEAR!"
-    break 
-    
 end
